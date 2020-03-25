119d37c0230b18feed30378b13640af18dce8e33
olvit <olvit2008@ukr.net>
2020-03-25 09:41:25 +0200

    Урок_37_5.7__5.8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,9 +86,9 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href=blog/posts>Посты</a>
+                    <a href=admin/blog/categories>Категории админка</a>
+                    <a href=admin/blog/posts>Посты админка</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
